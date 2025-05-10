d6b2d529fed2aa50ff627fdac89bcd613a45c976
Vikram <perfumesite08@gmail.com>
2025-05-11 03:47:20 +0530

    Initial commit: Clean CRA working version

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// src/main.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom'; // Needed for Vercel routing to not break SPA
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
